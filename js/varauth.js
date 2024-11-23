e7ecd89b12882c8caffd3eb71780e5a6ecf0c8ab
Pedro Andrade <105672579+pedroarltx@users.noreply.github.com>
2024-11-23 03:46:42 -0300

    Update

--- js/varauth.js
+++ js/varauth.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     // Verificar autenticação ao carregar a página
     firebase.auth().onAuthStateChanged((user) => {
         if (!user) {
-            // Se o usuário não estiver autenticado, redirecione para a página de login
-            window.location.href = "index.html";
+            // Se o usuário não estiver autenticado, redirecione imediatamente para a página de login
+            window.location.replace("index.html");  // usa replace para não manter a página atual no histórico
         }
     });
 });
